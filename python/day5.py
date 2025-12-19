b1f99a2042ff7df326384e2d9b7555b654c55697
Tres Sims <tres.sims95@example.com>
2025-12-18 20:03:56 -0600

    tweaker

--- python/day5.py
+++ python/day5.py
@@ -1,3 +1,17 @@
+class DataRange:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def getSize(self):
+        return self.end - self.start + 1
+
+    def __repr__(self):
+        return f"[{self.start:,}, {self.end:,}]"
+
+
+dataRanges = []
+
 with open("/home/tsims/Documents/aoc-2025/inputs/day5.txt") as f:
     input_file = f.read()
 
@@ -13,17 +27,26 @@ fresh_count = 0
 #             fresh_count += 1
 #             break
 
-codes = set()
-waitForInput = 0
-maxWait = 10000000
 for fresh_range in fresh:
     entries = [int(i) for i in fresh_range.split("-")]
-    for i in range(entries[0], entries[1] + 1):
-        codes.add(i)
-        waitForInput += 1
-        if waitForInput > maxWait:
-            input("Waiting for input")
-            waitForInput = 0
+    dataRanges.append(DataRange(entries[0], entries[1]))
 
+dataRanges.sort(key=lambda data: data.start)
 
-print(f"Day 5 - {len(codes)}")
+extraLookback = 1
+i = 1
+while i < len(dataRanges):
+    if dataRanges[i].start <= dataRanges[i - 1].end:
+        dataRanges[i].start = dataRanges[i - 1].end + 1
+
+    if dataRanges[i].start > dataRanges[i].end:
+        dataRanges.pop(i)
+        continue
+
+    i += 1
+
+for data in dataRanges:
+    fresh_count += data.getSize()
+
+print(dataRanges)
+print(f"Day 5 - {fresh_count}")
